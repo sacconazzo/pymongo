bca5675e9badba9a840b98b44fcc0f26d8af81cb
Giona Righini <32107095+sacconazzo@users.noreply.github.com>
2019-11-26 10:56:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from json import dumps
+from bson import json_util, ObjectId
+import json
 from flask_jsonpify import jsonify
 import pymongo
 
@@ -11,21 +12,33 @@ mycollection = mydb['scn']
 app = Flask(__name__)
 api = Api(app)
 
+
 class Employees(Resource):
     def get(self):
-        x = mycollection.count()
-        return jsonify(x)
+        x = mycollection.find()
+        page_sanitized = json.loads(json_util.dumps(x))
+        return page_sanitized
+        # return jsonify(x)
 
-class Employees_Name(Resource):
+class Employees_Filter(Resource):
+    def get(self, string_find):
+        x = mycollection.find({"name": {'$regex': string_find}})
+        page_sanitized = json.loads(json_util.dumps(x))
+        return page_sanitized
+        # return jsonify(x)
+
+class Employees_Insert(Resource):
     def get(self, employee_name):
         print(employee_name)
-        result = mycollection.insert_one({"name" : employee_name})
-        return jsonify(employee_name)
-        
+        result = mycollection.insert({"name": employee_name})
+        page_sanitized = json.loads(json_util.dumps(result))
+        return page_sanitized
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Employees_Name, '/employees/<employee_name>') # Route_3
+
+api.add_resource(Employees, '/employees')  # Route_1
+api.add_resource(Employees_Filter, '/employees/<string_find>')  # Route_1
+api.add_resource(Employees_Insert, '/employee/<employee_name>')  # Route_3
 
 
 if __name__ == '__main__':
-     app.run(port='5002')
+    app.run(port='5002')
