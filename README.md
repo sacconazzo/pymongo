227d26a294539afd7668d372a0f0b01bfae2bec1
Giona Righini <sapbasis@alteanet.it>
2019-11-26 19:56:35 +0100

    POST

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pymongo
 prove mongodb e flask
+
+- GET '/employees' - list
+- GET '/employees/<string_find>' - filter by name
+- GET '/employee/<employee_name>' - insert new name
+- POST '/employee' - insert new object with free properties
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import pymongo
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jsonpify import jsonify
 from bson import json_util, ObjectId
 import json
-from flask_jsonpify import jsonify
-import pymongo
 
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 mydb = myclient['mydatabase']
@@ -27,14 +27,21 @@ class Employees_Filter(Resource):
 
 
 class Employees_Insert(Resource):
-    def get(self, employee_name):
-        result = mycollection.insert({"name": employee_name})
+    def get(self, string_name):
+        result = mycollection.insert({"name": string_name})
+        return json.loads(json_util.dumps(result))
+
+
+class Employees_Insert_Complete(Resource):
+    def post(self):
+        result = mycollection.insert(json.loads(request.get_data()))
         return json.loads(json_util.dumps(result))
 
 
 api.add_resource(Employees, '/employees')  # Route_1
 api.add_resource(Employees_Filter, '/employees/<string_find>')  # Route_2
-api.add_resource(Employees_Insert, '/employee/<employee_name>')  # Route_3
+api.add_resource(Employees_Insert, '/employee/<string_name>')  # Route_3
+api.add_resource(Employees_Insert_Complete, '/employee')  # Route_4
 
 
 if __name__ == '__main__':
