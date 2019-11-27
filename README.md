31adee22ba0478696fe67d26fc09407af216847b
Giona Righini <sapbasis@alteanet.it>
2019-11-27 09:56:17 +0100

    Basic Auth

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # pymongo
-prove mongodb e flask
+prove restAPI mongodb e flask
 
 - GET '/employees' - list
 - GET '/employees/<string_find>' - filter by name
 - GET '/employee/<employee_name>' - insert new name
 - POST '/employee' - insert new object with free properties
+- DELETE '/employee/<oid>' - delete an object
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pymongo
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util, ObjectId
 import json
 
@@ -10,10 +12,24 @@ mydb = myclient['mydatabase']
 mycollection = mydb['scn']
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 api = Api(app)
+
+users = {
+    "sacconazzo": generate_password_hash("righini323"),
+    "scn": generate_password_hash("try")
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
 
 
 class Employees(Resource):
+    @auth.login_required
     def get(self):
         x = mycollection.find()
         return json.loads(json_util.dumps(x))
@@ -21,27 +37,38 @@ class Employees(Resource):
 
 
 class Employees_Filter(Resource):
+    @auth.login_required
     def get(self, string_find):
         x = mycollection.find({"name": {'$regex': string_find}})
         return json.loads(json_util.dumps(x))
 
 
 class Employees_Insert(Resource):
+    @auth.login_required
     def get(self, string_name):
         result = mycollection.insert({"name": string_name})
         return json.loads(json_util.dumps(result))
 
 
 class Employees_Insert_Complete(Resource):
+    @auth.login_required
     def post(self):
         result = mycollection.insert(json.loads(request.get_data()))
         return json.loads(json_util.dumps(result))
+
+
+class Employees_Delete(Resource):
+    @auth.login_required
+    def delete(self, oid):
+        mycollection.delete_one({'_id': ObjectId(oid)})
+        return "deleted", 204
 
 
 api.add_resource(Employees, '/employees')  # Route_1
 api.add_resource(Employees_Filter, '/employees/<string_find>')  # Route_2
 api.add_resource(Employees_Insert, '/employee/<string_name>')  # Route_3
 api.add_resource(Employees_Insert_Complete, '/employee')  # Route_4
+api.add_resource(Employees_Delete, '/employee/<oid>')  # Route_5
 
 
 if __name__ == '__main__':
